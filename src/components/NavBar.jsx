c62bcfb9bec2132924c1d9c32fd55cec85a53cf5
이운린 <105215297+likegitman@users.noreply.github.com>
2023-04-06 21:08:23 +0900

    delete ArticleItem
    overflow and h1 edit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,43 +6,33 @@ const NavBar = () => {
   return (
     <div className="navBar">
       <div className="navBox">
+        <div className="h1Box">
+          <h1>세월호 알아보기</h1>
+        </div>
         <ul>
-          <div className="h1Box">
-            <h1>세월호 알아보기</h1>
-          </div>
-          <ArticleItem id={"세월호 영상"} link={"세월호영상"} />
-          <ArticleItem id={"안전 교육 영상1"} link={"안전교육영상1"} />
-          <ArticleItem id={"안전 교육 영상2"} link={"안전교육영상2"} />
-          <ArticleItem id={"너를 보내고"} link={"너를보내고"} />
-          <ArticleItem id={"천개의 바람이 되어"} link={"천개의바람이되어"} />
+          <li>세월호 영상</li>
+          <li>안전 교육 영상1</li>
+          <li>안전 교육 영상2</li>
+          <li>너를 보내고</li>
+          <li>천개의 바람이 되어</li>
         </ul>
       </div>
       <div className="navBox">
+        <div className="h1Box">
+          <h1>퀴즈 풀어보기</h1>
+        </div>
         <ul>
-          <div className="h1Box">
-            <h1>퀴즈 풀어보기</h1>
-          </div>
-          <ArticleItem id={"문제 1"} />
-          <ArticleItem id={"문제 2"} />
-          <ArticleItem id={"문제 3"} />
-          <ArticleItem id={"문제 4"} />
-          <ArticleItem id={"문제 5"} />
-          <ArticleItem id={"문제 6"} />
-          <ArticleItem id={"문제 7"} />
-          <ArticleItem id={"문제 8"} />
-          <ArticleItem id={"문제 9"} />
-          <ArticleItem id={"문제 10"} />
+          <li>퀴즈1</li>
+          <li>퀴즈2</li>
+          <li>퀴즈3</li>
+          <li>퀴즈4</li>
+          <li>퀴즈5</li>
+          <li>퀴즈6</li>
+          <li>퀴즈7</li>
+          <li>퀴즈8</li>
+          <li>퀴즈9</li>
+          <li>퀴즈10</li>
         </ul>
-      </div>
-    </div>
-  );
-};
-
-const ArticleItem = ({ id }) => {
-  return (
-    <div>
-      <div className={({ isActive }) => (isActive ? "active" : undefined)}>
-        <li>{id}</li>
       </div>
     </div>
   );
