b4ac3af5ef887ee2e4df231413e0a0a0b4ced490
이운린 <105215297+likegitman@users.noreply.github.com>
2023-04-08 22:51:09 +0900

    video css error edit

--- src/components/video/SewolVideo.jsx
+++ src/components/video/SewolVideo.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Icons from "./icons/Icon";
+import ReactPlayer from "react-player/youtube";
 
 const SewolVideo = () => {
-  const URL = `https://www.youtube.com/watch?v=ALvTNrE_LbU&t=4s`;
+  const URL = "https://www.youtube.com/embed/ALvTNrE_LbU";
   return (
     <div>
       <Icons url={URL} />
       <iframe
-      className="video"
+        className="video"
         width="960"
         height="515"
         src={URL}
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-      ></iframe>
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share fullscreen"></iframe>
     </div>
   );
 };
