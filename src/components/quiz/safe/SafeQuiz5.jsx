8aacd8066f005820a1209e0581c1cc8764589a54
한준 <s22073@gsm.hs.kr>
2023-04-10 00:33:12 +0900

    quiz detail
    퀴즈 디테일 조정

--- src/components/quiz/safe/SafeQuiz5.jsx
+++ src/components/quiz/safe/SafeQuiz5.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import True from "../modal/True";
 import False from "../modal/False";
-import { FiCircle, FiX } from "react-icons/fi";
 
 const SafeQuiz5 = () => {
   const [trueOpen, setTrueOpen] = useState(false);
@@ -16,23 +15,27 @@ const SafeQuiz5 = () => {
   };
   return (
     <div>
-      <div>
-        <div className="quizBox">
-          <div className="quizInBox">
-            <h1>문제5</h1>
-            <p className="quiz1">
-              배가 기울어질때 선장이나 선원의 안내로 방 안에 그대로
-              머물러있는다.
-            </p>
-            <FiCircle className="circle" onClick={falseModal}/>
-            <FiX className="x" onClick={trueModal}/>
-          </div>
+      <div className="quizBox">
+        <div className="quizInBox">
+          <h1>문제5</h1>
+          <p className="quiz1">다음은 무엇을 설명한 것일까요?</p>
+          <p className="explain">(답을 선택해주세요)</p>
+          <p className="quizContent">
+            배나 건물 등에서 침수로 인해 공기가 남아 있는 부분을 말합니다. 이
+            공기가 선실 내 일부 또는 지하주차장 일부 공간에 남아 있는
+            현상입니다. 배가 뒤집혔을 때 선체 내부에 빠져나가지 못한 공기 방울을
+            일컫는 말이기도 합니다.
+          </p>
+          <p className="quizlist10">
+            <p onClick={trueModal}>1. 에어포켓</p>
+            <p onClick={falseModal}>2. 이안류</p>
+          </p>
         </div>
       </div>
       {trueOpen && (
         <True
           text="정답입니다!"
-          commentary="방안에 그대로 머물러 있는게 아닌 지시에 따른다."
+          commentary="이안류는 해안에서 바다로 흐르는 해류를 말합니다. 이안류는 보통 수십 미터에서 수백 미터까지 이어지며 유속은 최대 10km/h에 이르기도 합니다. 일반인의 수영 속도보다 훨씬 빠르기 때문에 절대로 이안류를 거슬러 헤엄쳐서 돌아오려고 하면 안 됩니다."
           setTrueOpen={setTrueOpen}
         />
       )}
@@ -40,7 +43,7 @@ const SafeQuiz5 = () => {
         <False
           text="오답입니다!"
           again="다시 풀어보세요"
-          commentary="방안에 그대로 머물러 있는게 아닌 지시에 따른다."
+          commentary="이안류는 해안에서 바다로 흐르는 해류를 말합니다. 이안류는 보통 수십 미터에서 수백 미터까지 이어지며 유속은 최대 10km/h에 이르기도 합니다. 일반인의 수영 속도보다 훨씬 빠르기 때문에 절대로 이안류를 거슬러 헤엄쳐서 돌아오려고 하면 안 됩니다."
           setFalseOpen={setFalseOpen}
         />
       )}
